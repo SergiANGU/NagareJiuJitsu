b94fad5b25ffe8d436d5fd4d49bc985f5afd4ca6
SergiANGU <107249354+SergiANGU@users.noreply.github.com>
2025-03-31 10:56:43 +0200

    vercel params i margenes laterales

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import { Menu } from "lucide-react";
 import { HeroSection } from "@/sections/HeroSection"
 import { AboutSection } from "@/sections/AboutSection"
 import { ClassesSection } from "@/sections/ClassesSection"
@@ -9,6 +8,7 @@ import { MapSection } from "@/sections/MapSection"
 import { FooterSection } from "@/sections/FooterSection"
 import { ContactSection } from "@/sections/ContactSection"
 import  NagareNombre from '@/assets/NagareNombre.svg';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 
@@ -25,12 +25,12 @@ function App() {
   return (
     <>
       <main className="bg-[#FAF9F6]" >
+      <SpeedInsights/>
         {/* Header */}
         <header
           className="flex items-center justify-between p-4 shadow-md sticky top-1 z-6 bg-[#FFA552] text-[#2C2C2C] rounded-lg">
              <img src={NagareNombre} alt="Logo Nagare" className="h-10 w-auto transform scale-450 origin-left" />
-          <Menu className="md:hidden" />
-          <nav className="hidden md:flex gap-6 text-sm">
+          <nav className="md:flex gap-6 text-md">
             <a href="#about">Sobre Nosotros</a>
             <a href="#classes">Clases</a>
             <a href="#faq">FAQ</a>
