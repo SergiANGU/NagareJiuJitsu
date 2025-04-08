123a3ec304986ef0f963a78bb4a45a7a91fe97db
SergiANGU <andresgutierrezsergi@gmail.com>
2025-04-08 19:18:17 +0200

    Estructuracion

---
+++ src/sections/MissionSection.tsx
@@ -0,0 +1,17 @@
+export function MissionSextion() {
+    return (
+        <section id="classes" className="py-20 px-4 sm:px-8 bg-[#FAF9F6] text-[#1F1F1F]">
+            <div>
+                <h3 className="text-3xl font-bold mb-4">Misión y visión</h3>
+                <ul className="grid gap-4 md:grid-cols-2">
+                    <li className="p-4 border rounded-xl shadow-sm bg-[#FFFFFF]">
+                        Nuestra vision es...
+                    </li>
+                    <li className="p-4 border rounded-xl shadow-sm bg-[#FFFFFF]">
+                        Nuestra Misión es...
+                        </li>
+                </ul>
+            </div>
+        </section>
+    );
+}
--- src/sections/TestimonialSection.tsx
+++ src/sections/TestimonialSection.tsx
@@ -1,24 +1,33 @@
 
 export function TestimonialsSection() {
-    return (
-        <section id="testimonios" className="py-20 px-4 sm:px-8 bg-[#FAF9F6] text-[#1F1F1F]">
-          <div className="max-w-4xl mx-auto">
-            <h3 className="text-3xl font-bold mb-8 text-center">Nuestros alumnos</h3>
-            <div className="grid gap-6 md:grid-cols-2">
-              <blockquote className="p-6 border-l-4 border-orange-300 shadow-sm bg-white">
-                <p className="text-lg italic">3r Copa Absoluta Española -69kg 2025</p>
-                <footer className="mt-2 text-md text-gray-600">Marc L.</footer>
-              </blockquote>
-              <blockquote className="p-6 border-l-4 border-orange-300 shadow-sm bg-white">
-                <p className="text-lg italic">Campeona absoluta de España 2022 y 2023</p>
-                <footer className="mt-2 text-md text-gray-600">Nuria A.</footer>
-              </blockquote>
-              <blockquote className="p-6 border-l-4 border-orange-300 shadow-sm bg-white">
-                <p className="text-lg italic">Campeón absoluto de Catalunya 2023</p>
-                <footer className="mt-2 text-md text-gray-600">Sergi A.</footer>
-              </blockquote>
-            </div>
-          </div>
-        </section>
-      );
-  }
+  return (
+    <section id="testimonios" className="py-20 px-4 sm:px-8 bg-[#FAF9F6] text-[#1F1F1F]">
+      <div className="max-w-4xl mx-auto">
+        <h3 className="text-3xl font-bold mb-8 text-center">Nuestros alumnos</h3>
+        <div className="grid gap-6 md:grid-cols-2">
+          <blockquote className="p-6 border-l-4 border-orange-300 shadow-sm bg-white">
+            <p className="text-lg italic">3r Copa Absoluta Española -69kg 2025</p>
+            <footer className="mt-2 text-md text-gray-600">Marc L.</footer>
+          </blockquote>
+          <blockquote className="p-6 border-l-4 border-orange-300 shadow-sm bg-white">
+            <p className="text-lg italic">Campeona absoluta de España 2022 y 2023</p>
+            <footer className="mt-2 text-md text-gray-600">Nuria A.</footer>
+          </blockquote>
+          <blockquote className="p-6 border-l-4 border-orange-300 shadow-sm bg-white">
+            <p className="text-lg italic">Campeón absoluto de Catalunya 2023</p>
+            <footer className="mt-2 text-md text-gray-600">Sergi A.</footer>
+          </blockquote>
+        </div>
+      </div>
+
+      <div className="max-w-4xl mx-auto mt-8">
+        <h3 className="text-3xl font-bold mb-8 text-center">Axel, nuestro sensei</h3>
+        <div className="md:col-span-2 flex justify-center">
+          <p className="text-lg text-gray-700 text-center max-w-xl">
+            Con una trayectoria de 30 años en el mundo del judo y jiujitsu...
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
