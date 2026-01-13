ac0722cc317ea2c662cfd301819ed9ab493580f2
SergiANGU <107249354+SergiANGU@users.noreply.github.com>
2026-01-13 17:00:32 +0100

    update FAQ and About text

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -4,10 +4,10 @@ export default function AboutSection() {
         <section id="about" className="py-20 px-4 sm:px-8 bg-[#F0EDE7] text-[#2C2C2C]">
         <h3 className="text-3xl font-bold mb-4">Sobre Nosotros</h3>
         <p className="text-lg text-gray-700">
-          Fusionamos el Jiu-Jitsu tradicional con el moderno.
+            Brazilean jiu jitsu i fagthing vertiente deportiva.
         </p>
         <p className="text-lg text-gray-700">
-          Trabajamos desde competicion, a autodefensa a recreativo.
+            Tanto competición como recreaciónnal
         </p>
       </section>
     );
--- src/sections/FAQSection.tsx
+++ src/sections/FAQSection.tsx
@@ -11,7 +11,7 @@ export default function FAQSection() {
             </div>
             <div>
               <h4 className="font-semibold">¿Qué ropa necesito para entrenar?</h4>
-              <p className="text-gray-700">Podeis venir con ropa deportiva cómoda. El gi, aunque mejor, no es obligatorio.</p>
+              <p className="text-gray-700">Podeis venir con ropa deportiva cómoda.</p>
             </div>
             <div>
               <h4 className="font-semibold">¿Puedo probar una clase gratis?</h4>
