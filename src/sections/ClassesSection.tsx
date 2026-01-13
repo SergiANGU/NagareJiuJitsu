58f11850ddc07d72ad1d9f86c1492c6bf2f4471d
SergiANGU <107249354+SergiANGU@users.noreply.github.com>
2026-01-13 16:46:13 +0100

    horarios clases

--- src/sections/ClassesSection.tsx
+++ src/sections/ClassesSection.tsx
@@ -6,10 +6,10 @@ export default function ClassesSection() {
           <h3 className="text-3xl font-bold mb-4">Clases</h3>
           <ul className="grid gap-4 md:grid-cols-2">
             <li className="p-4 border rounded-xl shadow-sm bg-[#FFFFFF]">
-                Sants:  Lunes, Miércoles y Viernes 20:30 - 22:00
+                Sants: Lunes a Viernes 13:00 - 14:00 y 20:30 - 22:00
                 </li>
                 <li className="p-4 border rounded-xl shadow-sm bg-[#FFFFFF]">
-                    Horta - Guinardó: Martes y Jueves 20:30 - 22:00</li>
+                    Horta - Guinardó: Martes y Jueves 20:00 - 21:30</li>
           </ul>
         </div>
       </section>
